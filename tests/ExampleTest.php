365bc3ea8e860d4224044c3cee665fd5c05ed980
Piotr Nguyen Ha <jimmy0699@gmail.com>
2022-05-26 12:29:57 +0200

    more tests

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,10 +2,18 @@
 
 use Sqrt\Sqrt;
 
-test('example', function ($value) {
+it('square root corectly', function ($value) {
     $r = ((new Sqrt)->pierwiastek($value, 10));
 
     expect($r)->toEqual(round(sqrt($value), 10));
 })->with([
-    0,1,3,4,5,7,9,231,526,-1, 9572.384
+    0, 0.000025,1,3,4,5,7,9,231,526, 9572.384
+]);
+
+it('square root negatives', function($value) {
+    $r = ((new Sqrt)->pierwiastek($value, 10));
+
+    expect($r)->toBeNan();
+})->with([
+    -1,-2
 ]);
